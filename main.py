075dd67ba72bde664d25b4ea1d75d834c2b8929b
123joshuawu <joshuawu3@gmail.com>
2020-10-07 16:44:51 -0400

    update for mypy

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+from dataclasses import dataclass
+from typing import Dict, Optional
+
 from fastapi import FastAPI
 
-app = FastAPI()
+app: FastAPI = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> Dict[str, str]:
     return {"Hello": "World"}
 
 
+@dataclass
+class Item:
+    item_id: int
+    q: Optional[str]
+
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int, q: Optional[str] = None) -> Item:
+    return Item(item_id, q)
